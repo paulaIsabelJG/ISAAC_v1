18687a9406d0ba3c382d4cd76ad11bdd0ea3adac
paulaIsabelJG <pijuanesgi.inf@upsa.es>
2026-06-08 09:45:46 +0200

    Conecta frontend con backend de Render

--- ISAAC_app1/src/environments/environment.prod.ts
+++ ISAAC_app1/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // ⚠️  Sustituye esta URL por la que Render asigne al backend tras el primer deploy.
-  // Formato: https://NOMBRE-DE-TU-BACKEND.onrender.com/api
-  apiUrl: 'https://isaac-backend.onrender.com/api',
+  apiUrl: 'https://isaac-backend-kbon.onrender.com/api'
 };
